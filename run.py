3a00b06f80d028934fc20e12b0b16bc13b4e8439
niall-code <niallcaplis@gmail.com>
2024-03-26 16:17:52 +0000

    Fix inaccurate sheet name argument in open method

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('cuisine_command')
+SHEET = GSPREAD_CLIENT.open('food_orders')
