fff4f67b182f5d4461e303823786662991c94561
niall-code <niallcaplis@gmail.com>
2024-03-27 15:44:44 +0000

    Create MenuItem and Order classes

--- README.md
+++ README.md
@@ -14,4 +14,10 @@ I created a Google Cloud project, enabled a Google Drive API, created credential
 
 I dragged the file of the JSON key into my workspace, renamed it 'creds', copied the client email address from it, shared my spreadsheet to that address, and added 'creds.json' to the gitignore.
 
-I installed gspread and google-auth in my workspace and imported them in the Python script. I pasted in the SCOPE constant, as used in Code Institute's "Love Sandwiches" walkthrough project. I used methods of Credentials (from google-auth) and of gspread to give my application access to the external spreadsheet, as demonstrated in the walkthrough project.
+I installed gspread and google-auth in my workspace and imported them in the Python script. I pasted in the SCOPE constant, as I had used during Code Institute's "Love Sandwiches" walkthrough project. I used methods of Credentials (from google-auth) and of gspread to give my application access to the external spreadsheet, as demonstrated in the walkthrough project.
+
+### Creating classes
+
+I added 'Name', 'Items ordered', and 'Cost' column headings to my spreadsheet.
+
+I created the MenuItem and Order classes. I defined the confirm method of the Order class. I added temporary dummy instances and a printed method call to test run the script so far, as captured from line 43 onward in my repository's fourth commit (the first commit on Mar 27, 2024).
--- run.py
+++ run.py
@@ -1,13 +1,47 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+# APIs connected to enable interaction with spreadsheet
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-
-CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('food_orders')
+
+class MenuItem:
+    '''
+    Create an instance of MenuItem.
+    '''
+    def __init__(self, foods, price):
+        self.foods = foods
+        self.price = price
+
+class Order:
+    '''
+    Create an instance of Order,
+    which shall be assigned to new_order.
+    '''
+    def __init__(self, name, items):
+        self.name = name
+        self.items = items
+
+    def confirm(self):
+        '''
+        For each MenuItem instance found in the list assigned to new_order's items attribute,
+        append all list values from that MenuItem's foods attribute to the order variable's
+        initially empty list. Return the new list.
+        '''
+        order = []
+        for item in self.items:
+            order.extend(item.foods)
+        return order
+
+test = MenuItem(['item1', 'item2', 'item3'], 12)
+test2 = MenuItem(['item4', 'item5', 'item6'], 12)
+
+new_order = Order('Geoffrey', [test, test2])
+print(new_order.confirm())
